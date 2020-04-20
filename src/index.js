e99749dd8afc509e63591e580e58bf0bf50d1ba4
unknown <alirezat92@me.com>
2020-04-20 16:53:29 +0430

    port

--- src/index.js
+++ src/index.js
@@ -25,6 +25,6 @@ if (!isProduction) {
 // routes
 app.use(require("./routes"));
 
-const server = app.listen(process.env.PORT || 4000, function () {
+const server = app.listen(process.env.PORT || 4000,'185.120.221.230', function () {
   console.log("Listening on port " + server.address().port);
 });
