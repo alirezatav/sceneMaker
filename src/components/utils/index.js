3c5a4ffeaddf950093443d0febea647e84b39825
unknown <alirezat92@me.com>
2020-04-20 19:11:19 +0430

    f

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -6,7 +6,6 @@ function trimMedia(input, output, start, duration) {
       .setStartTime(start / 1000)
       .setDuration(duration / 1000)
       .output(output)
-      .addInputOption("-vcodec copy -strict experimental  -c:a aac")
       .on("end", function (err) {
         if (!err) {
           res(true);
