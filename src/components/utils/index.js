b9c005fd0f93efa13aed92456bc80d8d922efd45
unknown <alirezat92@me.com>
2020-04-20 20:13:54 +0430

    f

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,6 +1,6 @@
-var ffmpeg = require("fluent-ffmpeg");
+// var ffmpeg = require("fluent-ffmpeg");
 var path = require("path");
-var ffmpeg2 = require('ffmpeg');
+var ffmpeg = require("ffmpeg");
 
 function trimMedia(input, output, start, duration) {
   console.log("output :", output);
@@ -24,24 +24,25 @@ function trimMedia(input, output, start, duration) {
       console.log(e.msg);
     }
 
-    let output =
-      "../../../resources/Movies/seven-world-one-planet-e1/scenes/hunt/hunt-1587396383112.mp4";
-    let input = "./";
-    ffmpeg(input)
-      .setStartTime(start / 1000)
-      .setDuration(duration / 1000)
-      .output(output)
-      .on("end", function (err) {
-        if (!err) {
-          res(true);
-        } else rej(false);
-      })
-      .on("error", function (err) {
-        rej(false);
-        console.log("FFMEPG ERROR: ", +err);
-      })
-      .run();
+    // let output =
+    //   "../../../resources/Movies/seven-world-one-planet-e1/scenes/hunt/hunt-1587396383112.mp4";
+    // let input = "./";
+    // ffmpeg(input)
+    //   .setStartTime(start / 1000)
+    //   .setDuration(duration / 1000)
+    //   .output(output)
+    //   .on("end", function (err) {
+    //     if (!err) {
+    //       res(true);
+    //     } else rej(false);
+    //   })
+    //   .on("error", function (err) {
+    //     rej(false);
+    //     console.log("FFMEPG ERROR: ", +err);
+    //   })
+    //   .run();
   });
 }
-
+// let command = `  ffmpeg -ss ${start} -i ${input} -c copy -t ${duration} -strict experimental ${output}  `;
+// executeFfmpeg(command)
 module.exports = { trimMedia };
