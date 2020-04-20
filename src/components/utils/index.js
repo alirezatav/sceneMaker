c364d13d5f19acbf7d41ab496e421a55ed6e521b
unknown <alirezat92@me.com>
2020-04-20 19:27:42 +0430

    f

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,15 +1,15 @@
 var ffmpeg = require("fluent-ffmpeg");
 
 function trimMedia(input, output, start, duration) {
-
-  console.log('output :',output);
-  console.log('input :',input);
-  console.log('duration :',duration);
-  console.log('start :',start);
+  console.log("output :", output);
+  console.log("input :", input);
+  console.log("duration :", duration);
+  console.log("start :", start);
   return new Promise((res, rej) => {
     ffmpeg(input)
-      // .setStartTime(start / 1000)
-      // .setDuration(duration / 1000)
+      .addInputOption("-strict experimental")
+      .setStartTime(start / 1000)
+      .setDuration(duration / 1000)
       .output(output)
       .on("end", function (err) {
         if (!err) {
