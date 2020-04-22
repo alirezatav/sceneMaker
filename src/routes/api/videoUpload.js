9bc75467557189e7dda27f80eb1639069f4c662a
unknown <alirezat92@me.com>
2020-04-22 15:30:41 +0430

    fIX something

--- src/routes/api/videoUpload.js
+++ src/routes/api/videoUpload.js
@@ -10,18 +10,18 @@ router.post("/", async (req, res) => {
       });
       return;
     }
-    if (!req.files.video||!req.files.subtitle) {
+    if (!req.files.video || !req.files.subtitle) {
       res.status(404).send({
         status: false,
         message: "No file uploaded",
       });
       return;
     } else {
-      let video = req.files.video.match(
+      let video = req.files.video.name.match(
         /^.*\.(mkv|mov|avi|wmv|flv|3gp|mp4|mpg)$/gi
       );
-      let subtitle = !!req.files.subtitle.match(/^.*\.(srt)$/gi)
-      
+      let subtitle = req.files.subtitle.name.match(/^.*\.(srt)$/gi);
+
       if (!subtitle || !video) {
         res.status(404).send({
           status: false,
@@ -31,11 +31,11 @@ router.post("/", async (req, res) => {
         return;
       }
 
-      let vPath = `${video.name}`;
-      let sPath = `${subtitle.name}`;
+      let vPath = `${req.files.video.name}`;
+      let sPath = `${req.files.subtitle.name}`;
       let category = req.body.category || "movie";
-      video.mv(`${moviesDirectory}/${vPath}`);
-      subtitle.mv(`${moviesDirectory}/${sPath}`);
+      req.files.video.mv(`${moviesDirectory}/${vPath}`);
+      req.files.subtitle.mv(`${moviesDirectory}/${sPath}`);
 
       let d = await insertVideo(
         req.body.name,
