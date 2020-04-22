42191a3c904c1f90d0c877467ec60fc9545bd3b2
unknown <alirezat92@me.com>
2020-04-22 05:08:44 +0430

    f

--- src/routes/api/getVideoWordScenes.js
+++ src/routes/api/getVideoWordScenes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 let getVideoWordScenes = require("../../components/utils/db.getVideoWordScenes");
-let static_url = require("./../../config").STATIC;
+ 
 router.get("/", async (req, res, next) => {
   let data = await getVideoWordScenes(req.query.id, req.query.word);
   res.status(200).json({
     status: true,
     data: data.rows.map((a) => ({
       ...a,
-      link: static_url + a.video_path,
+      link:   a.video_path,
     })),
   });
 });
